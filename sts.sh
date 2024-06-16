cecb48ce025aad51a66141a0cc597212b903e29c
lorenzhohermuth <lofloho@gmail.com>
2024-06-16 19:14:05 +0200

    creat executables

--- sts.sh
+++ sts.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+create_command () {
+	cmd_path="cmd/$1"
+	touch $cmd_path
+	echo "#!/bin/bash" > $cmd_path
+	echo "bash cmd/sys-links/$1-link" >> $cmd_path
+	chmod 770 $cmd_path
+}
+
 while test -n "$1"; do
 	case $1 in
 		-h|--help)
@@ -9,6 +17,7 @@ while test -n "$1"; do
 			command_name=$1
 			bash_file=$2
 			ln -srf "$bash_file" "cmd/sys-links/$command_name-link"
+			create_command "$command_name"
 			shift
 			;;
 	esac
